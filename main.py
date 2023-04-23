d1326e753cfdf80423807f5b8a3940d4a1794b24
Dyon <d.masrom@gmail.com>
2023-04-23 11:42:03 +0200

    Add model & update main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastApi
+import models
+from db import engine
 
+#create the database tables on app startup or reload
+models.Base.metadata.create_all(bind=engine)
 
 app = FastApi()
 
---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from db import Base
+
+#table
+
+class Items(Base):
+    __tablename__ = "Items"
+    id = Column(Integer, primary_key=True)
+    description = Column(String(150), nullable=False)
+    status = Column(String(10), nullable=False)
